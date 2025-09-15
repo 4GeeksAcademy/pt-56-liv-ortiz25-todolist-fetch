79c202797e90c02e034ffaf79e9fbffa0ec4b6a5
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2025-09-15 17:24:07 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const addTask = () => {
+    if (newTask.trim() === "") return; 
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="title">To-Do List</h1>
+        <div className="input">
+          <input
+            type="text"
+            size={50}
+            placeholder="What needs to be done?"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            onKeyDown={handleKeyDown} // 
+          />
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span>{task}</span>
+              <button onClick={() => removeTask(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+        {tasks.length > 0 ? (
+          <div className="note">
+            {tasks.length} item{tasks.length > 1 ? "s" : ""} left
+          </div>
+        ) : (
+          <p className="empty">No tasks, add a task</p>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default Home;
+
